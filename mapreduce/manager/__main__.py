1c562327f9e9b6691a76e906e76befc1f191a075
johnkyle <johnkyle@umich.edu>
2022-03-13 22:40:52 -0400

    3/13

--- mapreduce/manager/__main__.py
+++ mapreduce/manager/__main__.py
@@ -1,42 +1,133 @@
 """MapReduce framework Manager node."""
 import sys
+import pathlib
 import os
+import socket
 import logging
 import json
 import time
 import click
 import mapreduce.utils
-
+from threading import Thread
 
 # Configure logging
 LOGGER = logging.getLogger(__name__)
 
-#All communication done with strings formatted using JSON
+# All communication done with strings formatted using JSON
 class Manager:
     """Represent a MapReduce framework Manager node."""
-
-    def __init__(self, host, port, hb_port):
+    
+    def __init__(self, host, port, hb_port, workers):
         """Construct a Manager instance and start listening for messages."""
 
         LOGGER.info(
             "Starting manager host=%s port=%s hb_port=%s pwd=%s",
             host, port, hb_port, os.getcwd(),
-        )
-
-        # This is a fake message to demonstrate pretty printing with logging
-        message_dict = {
-            "message_type": "register",
-            "worker_host": "localhost",
-            "worker_port": 6001,
+          )
+        """
+        worker (worker_host, worker_port) = {
+            last_checkin (time)
+            status (ready, busy, dead)
+            worker_host
+            worker_port
         }
-        LOGGER.debug("TCP recv\n%s", json.dumps(message_dict, indent=2))
+        """
+        # set up tmp directory in mapreduce (mapreduce/tmp)
+        tmp_path = pathlib.Path(__file__).parents[1] / "tmp"
+        tmp_path.mkdir(exist_ok=True)
+        # delete any files that are in the dir (if already exists)
+        for file in tmp_path.glob('job-*'):
+            file.unlink()
+        # set up 
+        # make a heartbeat and fault tolerance thread, add to threads
+        hb_thread = Thread(target=Manager.manager_heartbeats)
+        hb_thread.start()
+        # Create an INET, STREAMing socket, this is TCP
+        # Note: context manager syntax allows for sockets to automatically be closed when an exception is raised or control flow returns.
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # Bind the socket to the server
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((host, port))
+            sock.listen()
+            # Socket accept() and recv() will block for a maximum of 1 second.  If you
+            # omit this, it blocks indefinitely, waiting for a connection.
+            sock.settimeout(1)
 
-        # TODO: you should remove this. This is just so the program doesn't
-        # exit immediately!
-        LOGGER.debug("IMPLEMENT ME!")
-        time.sleep(120)
+            while True:
+                # Wait for a connection for 1s.  The socket library avoids consuming
+                # CPU while waiting for a connection.
+                try:
+                    clientsocket, address = sock.accept()
+                except socket.timeout:
+                    continue
+                print("Connection from", address[0])
+                # Receive data, one chunk at a time.  If recv() times out before we can
+                # read a chunk, then go back to the top of the loop and try again.
+                # When the client closes the connection, recv() returns empty data,
+                # which breaks out of the loop.  We make a simplifying assumption that
+                # the client will always cleanly close the connection.
+                with clientsocket:
+                    message_chunks = []
+                    while True:
+                        try:
+                            data = clientsocket.recv(4096)
+                        except socket.timeout:
+                            continue
+                        if not data:
+                            break
+                        message_chunks.append(data)
+                # Decode list-of-byte-strings to UTF8 and parse JSON data
+                message_bytes = b''.join(message_chunks)
+                message_str = message_bytes.decode("utf-8")
+                try:
+                    message_dict = json.loads(message_str)
+                except json.JSONDecodeError:
+                    continue
+                print(message_dict)
 
 
+    def manager_heartbeats():
+        """Check for worker heartbeats on UDP."""
+        # Create an INET, DGRAM socket, this is UDP
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            # Bind the UDP socket to the server 
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((Manager.host, Manager.hb_port))
+            sock.settimeout(1)
+            # No sock.listen() since UDP doesn't establish connections like TCP
+            # Receive incoming UDP messages
+            while True:
+                # update worker times and check for death
+                curr_time = time.time()
+                for worker in Manager.workers:
+                    if worker['status'] != 'dead':
+                        if curr_time - worker['last_checkin'] > 12:
+                            ft_thread = Thread(target=Manager.manager_fault)
+                            worker['status'] = 'dead'
+                            
+                # check for any messages
+                try:
+                    message_bytes = sock.recv(4096)
+                except socket.timeout:
+                    continue
+                # if there is a message, 
+                message_str = message_bytes.decode("utf-8")
+                try:
+                    message_dict = json.loads(message_str)
+                except json.JSONDecodeError:
+                    continue
+                # update the time that the worker last checked in
+                key = (message_dict['worker_host'], message_dict['worker_port'])
+                # ignore if not registered 
+                if key not in Manager.workers.keys():
+                    continue
+                Manager.workers[key]['last_checkin'] = time.time()
+    
+    def manager_fault():
+        """Fault tolerance for managers."""
+        print('implement me')
+                    
+                
 @click.command()
 @click.option("--host", "host", default="localhost")
 @click.option("--port", "port", default=6000)
@@ -51,8 +142,7 @@ def main(host, port, hb_port):
     root_logger = logging.getLogger()
     root_logger.addHandler(handler)
     root_logger.setLevel(logging.INFO)
-    Manager(host, port, hb_port)
-
+    Manager(host, port, hb_port, {})
     """
     On startup manager should:
     
--- mapreduce/worker/__main__.py
+++ mapreduce/worker/__main__.py
@@ -6,6 +6,8 @@ import json
 import time
 import click
 import mapreduce.utils
+from threading import Thread
+import socket
 
 
 # Configure logging
@@ -33,11 +35,103 @@ class Worker:
             "worker_port": 6001,
         }
         LOGGER.debug("TCP recv\n%s", json.dumps(message_dict, indent=2))
+        """
+        On startup worker should:
 
-        # TODO: you should remove this. This is just so the program doesn't
-        # exit immediately!
+        Create a TCP socket on the port and call listen()
+        (Only one listen() for lifetime of worker)
+        ignore invalid (invalid is fail JSON Decoding)
+        try:
+            msg = json.loads(msg)
+        except JSONDecodeError:
+            continue
+
+        Send register message to manager (listening before sending this message)
+
+        when recieve Register_ack message, create a new thread for heartbeat messages (send to manager)
+
+        (Manager should ignore heartbeat from unregistered worker)
+        """
+        # create a TCP socket and listen()
+        """
+        
+        """
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # Bind the socket to the server
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((host, port))
+            sock.listen()
+            # Socket accept() and recv() will block for a maximum of 1 second.  If you
+            # omit this, it blocks indefinitely, waiting for a connection.
+            sock.settimeout(1)
+            while True:
+                # Wait for a connection for 1s.  The socket library avoids consuming
+                # CPU while waiting for a connection.
+                try:
+                    clientsocket, address = sock.accept()
+                except socket.timeout:
+                    continue
+                print("Connection from", address[0])
+                # Receive data, one chunk at a time.  If recv() times out before we can
+                # read a chunk, then go back to the top of the loop and try again.
+                # When the client closes the connection, recv() returns empty data,
+                # which breaks out of the loop.  We make a simplifying assumption that
+                # the client will always cleanly close the connection.
+                with clientsocket:
+                    message_chunks = []
+                    while True:
+                        try:
+                            data = clientsocket.recv(4096)
+                        except socket.timeout:
+                            continue
+                        if not data:
+                            break
+                        message_chunks.append(data)
+                # Decode list-of-byte-strings to UTF8 and parse JSON data
+                # message_str will be the register_ack
+                message_bytes = b''.join(message_chunks)
+                message_str = message_bytes.decode("utf-8")
+                try:
+                    message_dict = json.loads(message_str)
+                except json.JSONDecodeError:
+                    continue
+                print(message_dict)
+                # upon receiving he register_ack create the heartbeat thread
+                hb_thread = Thread(target=Worker.worker_heartbeats)
+                hb_thread.start()
+
+        # send the register message to the manager
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # connect to the server
+            sock.connect((manager_host, manager_port))
+            # send a message
+            register_message = {
+                "message_type" : "register",
+                "worker_host" : host,
+                "worker_port" : port
+            }
+            message = json.dumps(register_message)
+            sock.sendall(message.encode('utf-8'))
+
+{
+  "message_type": "register_ack",
+  "worker_host": string,
+  "worker_port": int,
+}
+
         LOGGER.debug("IMPLEMENT ME!")
         time.sleep(120)
+
+    def worker_heartbeats():
+        """Send worker heartbeats on UDP."""
+        # Create an INET, DGRAM socket, this is UDP
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            # Connect to the UDP socket on server
+            sock.connect((Worker.manager_host, Worker.manager_port))
+            # Send a message
+            hb_message = {}
+            message = json.dumps(hb_message)
+            sock.sendall(message.encode('utf-8'))
 
 
 @click.command()
@@ -57,24 +151,6 @@ def main(host, port, manager_host, manager_port, manager_hb_port):
     root_logger.addHandler(handler)
     root_logger.setLevel(logging.INFO)
     Worker(host, port, manager_host, manager_port, manager_hb_port)
-
-    """
-        On startup worker should:
-
-        Create a TCP socket on the port and call listen()
-        (Only one listen() for lifetime of worker)
-        ignore invalid (invalid is fail JSON Decoding)
-        try:
-            msg = json.loads(msg)
-        except JSONDecodeError:
-            continue
-
-        Send register message to manager (listening before sending this message)
-
-        when recieve Register_ack message, create a new thread for heartbeat messages (send to manager)
-
-        (Manager should ignore heartbeat from unregistered worker)
-    """
 
 if __name__ == '__main__':
     main()
